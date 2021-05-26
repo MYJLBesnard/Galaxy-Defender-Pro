caa1c2fb8f57db5162ccab5059994b86fd16455a
MYJLBesnard <myjl.besnard@gmaal.com>
2021-05-26 12:23:17 -0300

    Added the functions for shooting lasers and basic enemy gameobjects.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _enemyOneSpeed;
+
+    [SerializeField]
+    private GameObject _enemyOnePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyOneSpeed = Random.Range(2.5f, 4.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        transform.Translate(Vector3.down * _enemyOneSpeed * Time.deltaTime);
+
+        if (transform.position.y < -7.0f)
+        { 
+            float randomX = Random.Range(-8f, 8f);
+            transform.position = new Vector3(randomX, 7.0f, 0);
+            _enemyOneSpeed = Random.Range(2.5f, 4.5f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+         if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "LaserPlayer")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+
+    [SerializeField]
+    private int _lives = 3;
+
+    [SerializeField]
+    private GameObject _playerLaserPrefab;
+
+    [SerializeField]
+    private bool _hasPlayerLaserCooledDown = true;
+
+    [SerializeField]
+    private float _playerRateOfFire = 0.5f;
+
+
+    void Start()
+    {
+        transform.position = new Vector3(-5, 0, 0);
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && _hasPlayerLaserCooledDown)
+        {
+            PlayerFireLaser();
+        }
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+    void PlayerFireLaser()
+    {     
+            Instantiate(_playerLaserPrefab, transform.position + new Vector3(0, 0.7f, 0), Quaternion.identity);
+            _hasPlayerLaserCooledDown = false;
+            StartCoroutine(PlayerLaserCoolDownTimer());
+    }
+
+    IEnumerator PlayerLaserCoolDownTimer()
+    {
+        yield return new WaitForSeconds(_playerRateOfFire);
+        _hasPlayerLaserCooledDown = true;
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if (_lives < 1)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
