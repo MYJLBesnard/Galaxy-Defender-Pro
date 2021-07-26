8782e306a5adaa29ad9d10cd2c12522fd158c3aa
Michel YJL Besnard <81327793+MYJLBesnard@users.noreply.github.com>
2021-07-26 17:17:51 -0300

    New "Game Over" msg
    Added a variant to the Game Over UI Mnager portion which adds the text character by character.  Got the info, process, from Les.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -12,38 +12,29 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image _LivesImg;
 
     [SerializeField] private TMP_Text _readyText;
-
     [SerializeField] private TMP_Text _setText;
-
     [SerializeField] private TMP_Text _defendText;
-
-    [SerializeField] private TMP_Text _gameOverText;
-
     [SerializeField] private TMP_Text _pressToRestart;
+    [SerializeField] private TMP_Text _pressForMainMenu;
 
-    [SerializeField]
-    private TMP_Text _pressForMainMenu;
+    [SerializeField] private Sprite[] _livesSprite;
 
-    [SerializeField]
-    private Sprite[] _livesSprite;
+    [SerializeField] private GameManager _gameManager;
+    [SerializeField] private PlayerScript _player;
+    [SerializeField] private BoxCollider2D _playerBoxCollider2D;
+    [SerializeField] private SpawnManager _spawnManager;
 
-    [SerializeField]
-    private GameManager _gameManager;
-
-    [SerializeField]
-    private PlayerScript _player;
-
-    [SerializeField]
-    private BoxCollider2D _playerBoxCollider2D;
-
-    [SerializeField]
-    private SpawnManager _spawnManager;
-
+    [Header("Gam Over Display")]
+    [SerializeField] private TMP_Text _gameOverText;
+    [SerializeField] private float _letterDisplayDelay;
+    [SerializeField] private float _flashDelay;
+    [SerializeField] private int _flashCount;
 
     void Start()
     {
         _scoreText.text = "SCORE: " + 0;
-        _gameOverText.gameObject.SetActive(false);
+        //_gameOverText.gameObject.SetActive(false);
+        _gameOverText.enabled = false;
         _pressToRestart.gameObject.SetActive(false);
         _pressForMainMenu.gameObject.SetActive(false);
         _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
@@ -95,10 +86,46 @@ public class UIManager : MonoBehaviour
     private void GameOverSequence()
     {
         _gameManager.GameOver();
-        _gameOverText.gameObject.SetActive(true);
+        //_gameOverText.gameObject.SetActive(true);
+
+        _gameOverText.enabled = true;
+        string msg = _gameOverText.text;
+        _gameOverText.text = null;
+        StartCoroutine(GameOverRoutine(msg));
+
         _pressToRestart.gameObject.SetActive(true);
         _pressForMainMenu.gameObject.SetActive(true);
-        StartCoroutine(GameOverTextFlicker());
+        //StartCoroutine(GameOverTextFlicker());
+    }
+
+    IEnumerator GameOverRoutine(string msg)
+    {
+        yield return new WaitForSeconds(1.0f);
+        WaitForSeconds letterDelay = new WaitForSeconds(_letterDisplayDelay);
+        WaitForSeconds flashDelay = new WaitForSeconds(_flashDelay);
+
+        for (int i = 0; i<msg.Length; i++)
+        {
+            _gameOverText.text += msg[i].ToString();
+            yield return letterDelay;
+        }
+
+        bool flashGameOver = true;
+        int flashCount = 0;
+
+        while (flashGameOver)
+        {
+            yield return flashDelay;
+            _gameOverText.enabled = false;
+            yield return flashDelay;
+            _gameOverText.enabled = true;
+
+            flashCount++;
+            if (flashCount >= _flashCount)
+            {
+                flashGameOver = false;
+            }
+        }
     }
 
     IEnumerator CountDownToDefend()
