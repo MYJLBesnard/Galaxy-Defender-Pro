1f265b9d82506eb1af87f945f054b92ddfdedca1
MYJLBesnard <myjl.besnard@gmail.com>
2021-11-30 22:54:01 -0400

    Added Enemy Boss
    Added the Enemy Boss ship.  Added three laser turrets to the Boss, all three have rotating radar dishes to "track" Player.  Three turrets "track" Player and follow him as he moves around the Boss.
    Left to do:
    Implement Boss weapon systems (lasers from turrets, canons and mini-guns).
    Implement Boss Shield System.
    Change music for Boss Battle (?).
    Decide on Boss movement.

---
+++ Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TrackPlayer : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+  //  public GameObject homingMissileExplosionEffect;
+    [SerializeField] private GameObject _playerGameObject;
+    [SerializeField] private float _missileSpeed = 12.0f;
+    [SerializeField] private float _rotateSpeed = 350f;
+   // [SerializeField] private GameObject _selfdestructExplosion;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+            MoveTowardsEnemy();
+    }
+
+    private void MoveTowardsEnemy()
+    {
+        Vector2 direction = (Vector2)_playerGameObject.transform.position - _rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        _rb.angularVelocity = -rotateAmount * _rotateSpeed;
+        //_rb.velocity = transform.up * _missileSpeed;
+    }
+}
+
+
